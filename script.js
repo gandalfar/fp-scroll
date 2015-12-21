d9e8c264bb08e2ac17a840a79aafdae1e4cf38e7
Jure Cuhalev <gandalfar@gmail.com>
2015-12-21 18:23:16 +0100

    update prototype

--- script.js
+++ script.js
@@ -2,19 +2,54 @@ jQuery(document).ready(function($) {
 	var is_animating = false;
 
 	$(document).on("mousewheel", function(event) {
-		var windowHeight = $(window).height();
-		
 		if ( event.deltaY > 0 ) {
-		// scrolling up
-
-		$('.section-first').animate({
-			top: windowHeight * -1,
-		}, 1000);
-
+			handleUp();
 		} else if ( event.deltaY < 0 ) {
-		//scrolling down
 
 		}
 
 	});
+
+	
+	$(document.body).hammer().on("pandown", function(ev) {
+		handleUp();
+	});
+
+	var handleUp = function() {
+		if ( is_animating ) {
+			return;
+		}
+
+		is_animating = true;
+
+		var windowHeight = $(window).height();
+
+		if ( $('.section.visible').length > 1 ) {
+
+			$('.section.visible').first().animate({
+				top: windowHeight * -1,
+			}, {
+				duration: 1500,
+				complete: function() {
+					$(this).removeClass('visible');
+					is_animating = false;
+				}
+			});
+		} else {
+			$('.section.visible').first().animate({
+				top: windowHeight * -1,
+			},
+			{
+				duration: 1500,
+				queue: false,
+				complete: function() {
+					$(this).removeClass('visible');
+					is_animating = false;
+					$('.footer').css({height: 'auto', overflow: 'visible'});
+				}
+			});
+
+		}
+
+	};
 });
