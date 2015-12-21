35aa080087e32e1af50552800ec0a10a0fcf9804
Jure Cuhalev <gandalfar@gmail.com>
2015-12-21 17:37:29 +0100

    basic scroll

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 jQuery(document).ready(function($) {
-	
+	var is_animating = false;
+
+	$(document).on("mousewheel", function(event) {
+		var windowHeight = $(window).height();
+		
+		if ( event.deltaY > 0 ) {
+		// scrolling up
+
+		$('.section-first').animate({
+			top: windowHeight * -1,
+		}, 1000);
+
+		} else if ( event.deltaY < 0 ) {
+		//scrolling down
+
+		}
+
+	});
 });
